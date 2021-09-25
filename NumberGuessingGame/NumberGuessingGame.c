3795767f4bab3e17bbc4abcdae632d923434f983
Christopher Choi <cchoi1@cpp.edu>
2021-09-24 23:20:59 -0700

    initial commit: add source file

---
+++ NumberGuessingGame/NumberGuessingGame.c
@@ -0,0 +1,33 @@
+/*
+This program will have the user pick a number between 1 - 10.
+There will be a menu with three options:
+    - press 1 to play the game.
+        - if the user presses 1, they will play the game.
+        - if they guess correctly, the game will be done and they will return to menu.
+        - if incorrectly, then it will ask to guess again.
+        - game goes on until player wins.
+        - entering q will make them quit to menu.
+    - press 2 to change the max number.
+        - shows and lets the player change the max number.
+        - no negatives.
+        - no over max value.
+    - press 3 to quit.
+        -ends the program.
+Try to save it on a file.
+
+Steps:
+1.) include any necessary files
+2.) make main method
+    - declare variables and assign.
+3.) use while loops and if else statements.
+4.) make any necessary additional methods.
+5.) use file reading and writing to update a file that saves the max num the user saved.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main() {
+    
+}
